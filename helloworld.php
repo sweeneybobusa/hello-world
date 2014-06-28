e18e1416f1df98b2e9830c44dd2514d7b5855d16
sweeneybobusa <sweeneybobusa@gmail.com>
2014-06-28 19:58:57 -0400

    trying to committ files

---
+++ helloworld.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello World</title>
+</head>
+<body>
+  <p>
+       Today is <?php echo date('l F jS'); ?> and 
+       you are visiting <?php echo $_SERVER['SERVER_NAME']?>.
+  </p>
+</body>
+</html>
